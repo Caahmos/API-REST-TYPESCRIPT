1fbc4cb9f1d0355daaab0334e947987070fddbeb
unknown <dev.alexmorales@gmail.com>
2024-05-10 18:49:27 -0300

    Models Criadas com o Prisma

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 const app = express();
 import router from './src/routes/routes';
 
@@ -6,6 +7,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) return res.status(400).json({error: err.message});
+
+    return res.status(500).json({ status: 'error', message: 'Internal Server Error'})
+})
 
 app.listen(5000, () => {
     console.log('O servidor está rodando!');
